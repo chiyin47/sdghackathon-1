8c29e1be623b29a7ccdf0c579941abaedba8253c
chiyin47 <25005844@siswa.um.edu.my>
2025-12-01 12:45:33 +0800

    Change base path in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/sdghackathon-1",
+    base: "/C1G6_KMPH_SourceCode",
 });
